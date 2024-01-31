b94180a7b87bc8fbbeb1d0d05d4c708b1fe87c89
Rutger <rutger.pronk11@gmail.com>
2024-01-31 23:29:13 +0100

    Start integration form to email. Form needs refactoring regarding messages about wrong fields.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as process from "process";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://rutgerpronk.com"
+        : "http://localhost",
+  });
   await app.listen(3222);
 }
+
 bootstrap();
