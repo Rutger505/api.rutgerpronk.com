78c419389835fd74e37a0bba3183084361b4d1eb
Rutger <rutger.pronk11@gmail.com>
2024-02-05 20:12:52 +0100

    Fix: change api subdomain to be a path

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import * as process from "process";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? ["https://rutgerpronk.com", "https://rutgerpronk.nl"]
-        : "http://localhost",
-  });
+  app.setGlobalPrefix("api");
   await app.listen(3222);
 }
 
