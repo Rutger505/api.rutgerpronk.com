79dfb4a9a0e5139680208c97db0191e37fbeef7a
Rutger <rutger.pronk11@gmail.com>
2024-02-18 14:07:33 +0100

    Switch back to api.rutgerpronk.com

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,21 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix("api");
+
+  if (new Date() > new Date(2024, 1, 26)) {
+    console.warn(
+      "All origins are still allowed due to school escape room project. Change to rutgerpronk.com",
+    );
+  }
+  app.enableCors({
+    origin: "*",
+  });
+  // app.enableCors({
+  //   origin:
+  //     process.env.NODE_ENV === "production"
+  //       ? "https://rutgerpronk.com"
+  //       : "http://localhost",
+  // });
   await app.listen(3222);
 }
 
